d9a6713f23401beecbc467cb0ce1f038ca9b4285
webwithsumit <sumit2410kushwaha@gmail.com>
2025-02-23 11:11:11 +0530

    Sign in page

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+const SignIn = () => {
+  return (
+    <div className='text-xl px-10 sm:px-0 flex items-center justify-center min-h-[50vh]'>
+      <h1>
+        <span className='font-semibold text-4xl text-left'>Sign In Page</span> <br />
+        But why do you even wan't to sign in here , Huh ? 🧐
+      </h1>
+    </div>
+  )
+}
+
+export default SignIn
