79316598803851882d4c9dcef764a1fa29874f78
webwithsumit <sumit2410kushwaha@gmail.com>
2025-02-24 11:11:11 +0530

    Loading page

---
+++ src/pages/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+    return (
+        <div className='h-screen w-full flex items-center justify-center'>
+            <h1 className='font-semibold text-3xl'>Loading...</h1>
+        </div>
+    )
+}
+
+export default Loading
