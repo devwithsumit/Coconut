fd735f707e855d388b90f67b5892f41eaf47b82d
webwithsumit <sumit2410kushwaha@gmail.com>
2025-03-26 11:11:11 +0530

    Home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,75 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import SearchBar from '../components/home/SearchBar';
-import axios from 'axios';
 import ResCard, { PromotedResCard } from '../components/home/resCard';
-import { useDispatch, useSelector } from 'react-redux';
-import { loadData } from '../redux/actions/restaurantActions';
+import { useSelector } from 'react-redux';
 import Loading from './Loading';
+import SearchPanel from './SearchPanel';
+import { searchedRes } from '../utils/searchedRes';
 
 // import ShimmerResCard from '../components/home/ShimmerResCard';
 // import useRestaurantInfo from '../utils/useRestaurantInfo';
 
 const Home = () => {
     const [searchText, setSearchText] = useState("");
-
-    const dispatch = useDispatch();
-
+    const [searchBarActive, setSearchBarActive] = useState(false);
+    const searchAreaRef = useRef(null);
+    const [searchList, setSearchList] = useState([]);
     // extracting data as sections
     const { loading, data: sections } = useSelector((state) => state.home);
+
+    const data = useSelector(state => state.home.data);
+    //Search Logic
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const fetchedSearchList = searchedRes(data, searchText);
+        setSearchList(fetchedSearchList)
+        // console.log(searchText);
+    }
+    // const handleChange = (e) => {
+    // }
+    // useEffect(() => {
+    //     if (searchText.length === 0) {
+    //         setSearchBarActive(false);
+    //     }
+    // }, [searchText])
 
     // ResCard with promoted badge
     const EnhancedCard = PromotedResCard(ResCard);
 
+    // Close panel when clicking outside
+    useEffect(() => {
+        function handleClickOutside(event) {
+            // If clicked element is NOT inside searchAreaRef, close panel
+            if (searchAreaRef.current && !searchAreaRef.current.contains(event.target)) {
+                setSearchBarActive(false);
+            }
+        }
+        // Add event listener when component mounts
+        document.addEventListener('mousedown', handleClickOutside);
+
+        // Clean up event listener when component unmounts
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
     return (
-        <div className='px-5 py-5 md:px-10 h-full'>
-            <div className="search-bar-area w-full sm:px-20 md:px-20">
-                <SearchBar placeholderText={'Search for Restaurants'} searchText={searchText} setSearchText={setSearchText} />
+        <div className='px-4 py-5 md:px-10 h-full relative'>
+            <div className="search-area" ref={searchAreaRef}>
+                <div className="search-bar-area w-full sm:px-20 md:px-20">
+                    <div
+                        onClick={() => setSearchBarActive(true)}
+                        className="search-bar-wrapper"
+                    >
+                        <SearchBar placeholderText={'Search for Restaurants'} searchText={searchText} setSearchText={setSearchText} handleSubmit={handleSubmit} />
+                    </div>
+                </div>
+                {searchBarActive && (
+                    <div className="search-section-wrapper bg-gray-100 z-[999] p-2 sm:px-20 absolute shadow w-[90%] left-1/2 -translate-x-1/2 ">
+                        <SearchPanel searchList={searchList} />
+                    </div>
+                )}
             </div>
             {loading && <Loading />}
-
             <div className='md:px-20'>
                 {sections?.length > 0 && (
                     sections.map((section, index) => (
