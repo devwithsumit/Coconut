2ef72a56a9f6ebd3a1a9cb29b5c0d21c6e85763d
webwithsumit <sumit2410kushwaha@gmail.com>
2025-04-05 11:11:11 +0530

    git ignore and other additional updates

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+        extend: {
+            screens: {
+                'xs': '375px', // Add a custom breakpoint
+            }
+        }
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -12,5 +14,11 @@ export default defineConfig({
     proxy : {
       '/dapi' : 'http://localhost:5173',
     }
+  },
+  test:{
+     globals: true,
+     environment: 'jsdom',
+     css: true,
+     setupFiles: './src/test/setup.js'
   }
 })
